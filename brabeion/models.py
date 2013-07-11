563e8516983b25bfb5a9a558f7e411d716ae47a8
Murph Murphy <omurcada@gmail.com>
2013-07-11 14:17:22 -0600

    * Re-support Django 1.3

--- brabeion/models.py
+++ brabeion/models.py
@@ -2,36 +2,34 @@ from datetime import datetime
 
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
-
 
 
 class BadgeAward(models.Model):
     user = models.ForeignKey(User, related_name="badges_earned")
-    awarded_at = models.DateTimeField(default=timezone.now)
+    awarded_at = models.DateTimeField(default=datetime.now)
     slug = models.CharField(max_length=255)
     level = models.IntegerField()
-    
+
     def __getattr__(self, attr):
         return getattr(self._badge, attr)
-    
+
     @property
     def badge(self):
         return self
-    
+
     @property
     def _badge(self):
         from brabeion import badges
         return badges._registry[self.slug]
-    
+
     @property
     def name(self):
         return self._badge.levels[self.level].name
-    
+
     @property
     def description(self):
         return self._badge.levels[self.level].description
-    
+
     @property
     def progress(self):
         return self._badge.progress(self.user, self.level)
